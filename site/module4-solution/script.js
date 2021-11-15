a3ed87f69f28b875b3c74e4781370fb9ea8cb838
4265257 <92646375+4265257@users.noreply.github.com>
2021-11-15 15:37:08 -0800

    Update script.js

--- site/module4-solution/script.js
+++ site/module4-solution/script.js
@@ -21,6 +21,10 @@ Hello Paula
 Hello Laura
 Good Bye Jim
 
+
+
+
+
 WARNING!!! WARNING!!!
 The code does NOT currently work! It is YOUR job to make it work
 as described in the requirements and the steps in order to complete this
@@ -33,9 +37,11 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+
+
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
@@ -64,4 +70,4 @@ for (var i = 0; i < names.length; i++) {
   }
 }
 
-})();
+})()
